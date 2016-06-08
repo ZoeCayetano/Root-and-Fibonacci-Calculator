c58cb3c2af2a286048c459c32fa3765f0f55ac9d
Zoë Cayetano <zcayetan@asu.edu>
2016-06-07 23:05:23 -0700

    Create myMath.hpp

---
+++ MyMath.hpp
@@ -0,0 +1,19 @@
+//**************************************************************************************************************
+// FILE:        MyMath.hpp
+//
+// DESCRIPTION: Contains MyMath header file.
+//
+// AUTHORS:     Zoe Cayetano            (zcayetan@asu.edu)
+//
+// COURSE:      CSE100 Principles of Programming with C++, Spring 2016
+//
+// LAB INFO:    Lab 12   Date/Time: 12pm Th   TA: Suya
+//**************************************************************************************************************
+
+#ifndef MYMATH_HPP
+#define MYMATH_HPP
+
+double athRoot(double n, int a, double epsilon);
+int fib(int n);
+
+#endif
